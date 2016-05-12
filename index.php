c00e44538ffb085c571fb1c1c34c019e00d48a57
ZiQing Li <pianweiwan0413@163.com>
2016-05-12 18:36:21 +0800

    no message

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ziqingli
+ * Date: 2016/5/11
+ * Time: 10:41
+ */
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', __DIR__.DS);
+$uri = $_SERVER['REQUEST_URI'];
+ini_set('date.timezone','Asia/Shanghai');
+require_once('config/func.php');
+
+//引入配置
+import('config');
+//自动加载类
+import('autoload');
+
+
+
+
